162ca7e28297344487abc1e603c66831b884dc4d
Leandra Kim & Sara Gilford <pair+lea3182.saragilford@devbootcamp.com>
2015-05-01 18:37:26 -0700

    Complete ajax for questions

--- backend/app/controllers/questions_controller.rb
+++ backend/app/controllers/questions_controller.rb
@@ -4,4 +4,23 @@ class QuestionsController < ApplicationController
       @questions = Question.all
       render json: @questions
   end
+
+  def create
+    p "*" *80
+    @question = Question.new(title: params[:title], content: params[:content])
+    if @question.save
+      p "3*" *80
+      render json: @question
+    else
+      render json: @question.errors
+    end
+
+  end
+
+  # private
+
+  # def question_params
+  #   p params
+  #   params.require(:question).permit(:title, :content)
+  # end
 end
